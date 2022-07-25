a4bed9625d102513ac6e7bcfe96e405ed79ded03
Valentin <marian.rusoiu99@e-uvt.ro>
2022-07-25 14:23:26 +0300

    sddsfsd

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    
+
+  
+    return(
+        <div className="header">
+            <img src="../images/Logo.png" alt= "meme"  className="header--logo"/>
+            <h1 className="header--title"> React Course - Project 3</h1>
+        </div>
+        
+        )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./components/App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+// function good (name){
+//     const date = new  Date();
+//     const hours = date.getHours();
+//     if( hours> 4 &&hours <12){
+//         console.log("good morning " + name);
+//     }
+    
+//     if( hours> 12 &&hours <17){
+//         console.log("good afternoon " + name);
+//     }
+    
+//     if( hours> 17 &&hours <20){
+//         console.log("good evening " + name);
+//     }
+    
+//     if( hours> 20 &&hours <4){
+//         console.log("good night " + name);
+//     }
+// }
+// good("bob");
+
+ 
+
+const container = document.getElementById('root');
+const root = createRoot(container); 
+
+
+
+root.render(<App tab="home"/>);
